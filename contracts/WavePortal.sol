9084f74378df466ab3770a05760e2f0cf87717b8
Temitope Adegoke <adegoketemitope1909@gmail.com>
2022-04-08 07:24:05 +0100

    first commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo, I'm a contract, writing in Solidity with the help of Hardhat and I am smart!");
+    }
+}
