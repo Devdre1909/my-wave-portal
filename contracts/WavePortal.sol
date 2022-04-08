4bad16d73902fe0b7503a9bc1ad9c9ba5061d89c
Temitope Adegoke <adegoketemitope1909@gmail.com>
2022-04-08 08:12:34 +0100

    feat: wave and count total waves

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,20 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    uint totalWaves = 0;
+
     constructor() {
         console.log("Yo, I'm a contract, written in Solidity with the help of Hardhat and I am smart!");
+    }
+
+    function wave() public {
+        totalWaves++;
+        console.log("%s just waved!", msg.sender);
+    }
+
+    function getWaves() public view returns (uint) {
+       console.log("%s accounts has waved", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,31 @@
 // Compile
 
 const main = async () => {
+  const [owner, randomPerson1, randomPerson2] = await hre.ethers.getSigners()
+  
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
   console.log("Contract deployed to", waveContract.address);
+  console.log("Contract deployed by", owner.address);
+
+  let waveCount
+  waveCount = await waveContract.getWaves();
+  
+  let waveTxn = await waveContract.wave()
+  await waveTxn.wait()
+
+  waveCount = await waveContract.getWaves();
+
+  waveTxn = await waveContract.connect(randomPerson1).wave()
+  await waveTxn.wait()
+
+  waveTxn = await waveContract.connect(randomPerson2).wave()
+  await waveTxn.wait()
+
+  waveCount = await waveContract.getWaves();
+
 }
 
 // Deploy
