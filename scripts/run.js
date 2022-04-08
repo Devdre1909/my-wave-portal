c07e5d23aca59feefde479ac4213fea1a27c5da4
Temitope Adegoke <adegoketemitope1909@gmail.com>
2022-04-08 07:46:59 +0100

    feat: compile and deploy scripts

---
+++ scripts/run.js
@@ -0,0 +1,22 @@
+// Compile
+
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Contract deployed to", waveContract.address);
+}
+
+// Deploy
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (e) {
+    console.log(e)
+    process.exit(1)
+  }
+}
+
+// Run
+runMain();
